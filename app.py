86dd498d0a7b9d056b30b0c6db70fd6e49e6409d
marcusrein <64141593+marcusrein@users.noreply.github.com>
2020-05-01 06:02:19 -0400

    rearranged a few things. still working homepage.

--- app.py
+++ app.py
@@ -4,17 +4,8 @@ from flask import Flask, render_template, redirect, url_for, request
 # create the application object
 app = Flask(__name__)
 
-# use decorators to link the function to a url
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return "Hello, World!"  # return a string
-
-@app.route('/welcome')
-def welcome():
-    return render_template('welcome.html')  # render a template
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
@@ -22,6 +13,15 @@ def login():
         else:
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
+
+# use decorators to link the function to a url
+# @app.route('/')
+# def home():
+#     return "Hello, World!"  # return a string
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')  # render a template
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
